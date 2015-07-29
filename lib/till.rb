8af3f7cc9ab6d3efc9c26d0afe3af7c43cf5ca76
smarbaf <smarbaf@hotmail.com>
2015-07-29 17:50:17 +0100

    Can add item; test passing

--- lib/till.rb
+++ lib/till.rb
@@ -1,2 +1,13 @@
 class Till
+
+  attr_reader :items
+
+  def initialize
+    @items = []
+  end
+
+  def add(item)
+    items << item
+  end
+
 end
--- spec/till_spec.rb
+++ spec/till_spec.rb
@@ -2,8 +2,8 @@ require 'till'
 
 describe Till do
   it "can add an item" do
-    subject.add_item(:tea)
-    expect(subject.items).to eq(:tea)
+    subject.add(:tea)
+    expect(subject.items).to eq [:tea]
   end
 
 end
