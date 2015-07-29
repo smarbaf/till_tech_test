dac692f379c25e256204fb33bcd6fc3505030dfe
smarbaf <smarbaf@hotmail.com>
2015-07-29 17:11:04 +0100

    Changed till to subject; added class Till

--- spec/till_spec.rb
+++ spec/till_spec.rb
@@ -2,8 +2,8 @@ require 'till'
 
 describe Till do
   it "can add an item" do
-    till.add_item(:tea)
-    expect(till.items).to eq(:tea)
+    subject.add_item(:tea)
+    expect(subject.items).to eq(:tea)
   end
 
 end
